40aa2452b8566abd7d4f15c3e6e010ccd5404f61
Geuthur <geuthur@users.noreply.github.com>
2024-06-28 10:29:18 +0200

    Update Changelog & Push to 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## \[0.1.1\] - 2024-06-28
+
+### Changed
+
+- Moved JS to File
+
 ## \[0.1.0\] - 2024-06-24
 
 ### Added
--- killstats/__init__.py
+++ killstats/__init__.py
@@ -1,6 +1,6 @@
 """Initialize the app"""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __title__ = "Killstats"
 
 APP_NAME = "killstats"
