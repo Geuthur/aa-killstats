c80748edf363451b2cf8aab30988e2eafa62c623
Geuthur <761682+Geuthur@users.noreply.github.com>
2024-07-20 14:01:31 +0200

    bump to 0.1.6

--- killstats/tests/test_api/test_killstatsapi.py
+++ killstats/tests/test_api/test_killstatsapi.py
@@ -7,7 +7,7 @@ from django.test import TestCase
 from app_utils.testing import create_user_from_evecharacter
 
 from killstats.api.killboard import KillboardApiEndpoints
-from killstats.tests.test_api.killstasts_api import KillstatsMonth
+from killstats.tests.test_api._killstasts_api import KillstatsMonth
 from killstats.tests.testdata.load_allianceauth import load_allianceauth
 from killstats.tests.testdata.load_killstats import load_killstats_all
 
