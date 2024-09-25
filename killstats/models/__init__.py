8f74bcaa85c67024dc1ae3fcb48eef0f3d5863a8
Geuthur <761682+Geuthur@users.noreply.github.com>
2024-09-25 21:52:05 +0200

    [MISC] Add Attacker

--- killstats/models/__init__.py
+++ killstats/models/__init__.py
@@ -1,3 +1,3 @@
-from .general import EveEntity, General
-from .killboard import Killmail
+from .general import General
+from .killboard import Attacker, Killmail
 from .killstatsaudit import AlliancesAudit, CorporationsAudit
