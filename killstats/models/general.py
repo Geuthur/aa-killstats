e3d1b53f7aede0678424827c3b3635da03159c44
Geuthur <761682+Geuthur@users.noreply.github.com>
2024-09-25 18:45:11 +0200

    [REMOVE] Remove EveEntity model use django-eveuniverse from now on

--- killstats/models/general.py
+++ killstats/models/general.py
@@ -1,22 +1,13 @@
 # Django
 from django.contrib.auth.models import Permission
-from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
-# Alliance Auth
-from allianceauth.eveonline.models import (
-    EveAllianceInfo,
-    EveCharacter,
-    EveCorporationInfo,
-)
 
 # Alliance Auth (External Libs)
 from app_utils.django import users_with_permission
 
 # AA Killstats
 from killstats.hooks import get_extension_logger
-from killstats.managers.general_manager import EveEntityManager
 
 logger = get_extension_logger(__name__)
 
@@ -43,85 +34,3 @@ class General(models.Model):
     def users_with_basic_access(cls) -> models.QuerySet:
         """Return users which have at least basic access to Voices of War."""
         return users_with_permission(cls.basic_permission())
-
-
-class EveEntity(models.Model):
-    """An Eve entity like a corporation or a character"""
-
-    CATEGORY_ALLIANCE = "alliance"
-    CATEGORY_CHARACTER = "character"
-    CATEGORY_CORPORATION = "corporation"
-
-    CATEGORY_CHOICES = (
-        (CATEGORY_ALLIANCE, "Alliance"),
-        (CATEGORY_CORPORATION, "Corporation"),
-        (CATEGORY_CHARACTER, "Character"),
-    )
-
-    eve_id = models.IntegerField(
-        primary_key=True, validators=[MinValueValidator(0)], verbose_name=_("id")
-    )
-    category = models.CharField(
-        max_length=32, choices=CATEGORY_CHOICES, verbose_name=_("category")
-    )
-    name = models.CharField(max_length=254, verbose_name=_("name"))
-
-    corporation = models.ForeignKey(
-        "EveEntity",
-        on_delete=models.SET_NULL,
-        null=True,
-        default=None,
-        related_name="corp",
-    )
-    alliance = models.ForeignKey(
-        "EveEntity",
-        on_delete=models.SET_NULL,
-        null=True,
-        default=None,
-        related_name="alli",
-    )
-    last_update = models.DateTimeField(auto_now=True)
-
-    objects = EveEntityManager()
-
-    def __str__(self):
-        return self.name
-
-    def __repr__(self) -> str:
-        return (
-            f"{self.__class__.__name__}(id={self.eve_id}, category='{self.category}', "
-            f"name='{self.name}')"
-        )
-
-    @property
-    def is_alliance(self) -> bool:
-        """Return True if entity is an alliance, else False."""
-        return self.category == self.CATEGORY_ALLIANCE
-
-    @property
-    def is_corporation(self) -> bool:
-        """Return True if entity is an corporation, else False."""
-        return self.category == self.CATEGORY_CORPORATION
-
-    @property
-    def is_character(self) -> bool:
-        """Return True if entity is a character, else False."""
-        return self.category == self.CATEGORY_CHARACTER
-
-    def icon_url(self, size=128) -> str:
-        """Url to an icon image for this organization."""
-        if self.category == self.CATEGORY_ALLIANCE:
-            return EveAllianceInfo.generic_logo_url(self.eve_id, size=size)
-
-        if self.category == self.CATEGORY_CORPORATION:
-            return EveCorporationInfo.generic_logo_url(self.eve_id, size=size)
-
-        if self.category == self.CATEGORY_CHARACTER:
-            return EveCharacter.generic_portrait_url(self.eve_id, size=size)
-
-        raise NotImplementedError(
-            f"Avatar URL not implemented for category {self.category}"
-        )
-
-    class Meta:
-        default_permissions = ()
