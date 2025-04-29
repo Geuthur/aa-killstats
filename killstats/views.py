3e7653db490f30d999cf163faed7da2ecb8888e1
Geuthur <geuthur@users.noreply.github.com>
2025-04-29 07:36:29 +0200

    [CHANGE] Refactor ADD Corp & Alliance

--- killstats/views.py
+++ killstats/views.py
@@ -18,7 +18,6 @@ from allianceauth.eveonline.providers import ObjectNotFound, provider
 from killstats import __title__
 from killstats.models.killboard import Killmail
 from killstats.models.killstatsaudit import AlliancesAudit, CorporationsAudit
-from killstats.tasks import killmail_update_ally, killmail_update_corp
 
 from .hooks import get_extension_logger
 
@@ -48,7 +47,7 @@ def corporation_view(request, corporation_id=None):
     )[:5]
 
     context = {
-        "title": "Corporation Killboard",
+        "title": "Corporation Killstats",
         "years": years,
         "entity_pk": corporation_id,
         "entity_type": "corporation",
@@ -77,7 +76,7 @@ def alliance_view(request, alliance_id=None):
     )[:5]
 
     context = {
-        "title": "Alliance Killboard",
+        "title": "Alliance Killstats",
         "years": years,
         "entity_pk": alliance_id,
         "entity_type": "alliance",
@@ -89,35 +88,30 @@ def alliance_view(request, alliance_id=None):
 @token_required(scopes=("publicData"))
 @permission_required(["killstats.admin_access"])
 def add_corp(request, token):
-    char = EveCharacter.objects.get_character_by_id(token.character_id)
-    if char:
-        # Check if it is a NPC Corporation
-        if char.corporation_id < 10_000_000:
-            msg = "Cannot add NPC Corporation to Killboard"
-            messages.error(request, msg)
-            return redirect("killstats:index")
+    char = get_object_or_404(EveCharacter, character_id=token.character_id)
 
-        corp, _ = EveCorporationInfo.objects.get_or_create(
-            corporation_id=char.corporation_id,
-            defaults={
-                "member_count": 0,
-                "corporation_ticker": char.corporation_ticker,
-                "corporation_name": char.corporation_name,
-            },
-        )
-
-        _, created = CorporationsAudit.objects.update_or_create(
-            corporation=corp, owner=char
-        )
-        if created:
-            killmail_update_corp.apply_async(args=[char.corporation_id], priority=6)
-        msg = f"{char.corporation_name} successfully added/updated to Killboard"
-        messages.info(request, msg)
+    # Check if it is a NPC Corporation
+    if char.corporation_id < 10_000_000:
+        msg = "Cannot add NPC Corporation to Killstats"
+        messages.error(request, msg)
         return redirect("killstats:index")
 
-    msg = "Failed to add Corporation to Killboard"
-    messages.error(request, msg)
-    return redirect("killstats:index")
+    corp, _ = EveCorporationInfo.objects.get_or_create(
+        corporation_id=char.corporation_id,
+        defaults={
+            "member_count": 0,
+            "corporation_ticker": char.corporation_ticker,
+            "corporation_name": char.corporation_name,
+        },
+    )
+
+    audit, __ = CorporationsAudit.objects.update_or_create(corporation=corp, owner=char)
+
+    msg = (
+        f"{audit.corporation.corporation_name} successfully added/updated to Killstats"
+    )
+    messages.info(request, msg)
+    return redirect("killstats:corporation", corporation_id=corp.corporation_id)
 
 
 @login_required
@@ -127,33 +121,30 @@ def add_alliance(request, token):
     char = get_object_or_404(EveCharacter, character_id=token.character_id)
 
     try:
-        alliance = EveAllianceInfo.objects.get(alliance_id=char.alliance_id)
-    except EveAllianceInfo.DoesNotExist:
-        try:
-            ally_data = provider.get_alliance(char.alliance_id)
-            alliance, __ = EveAllianceInfo.objects.get_or_create(
-                alliance_id=ally_data.id,
-                defaults={
-                    "alliance_name": ally_data.name,
-                    "alliance_ticker": ally_data.ticker,
-                    "executor_corp_id": ally_data.executor_corp_id,
-                },
-            )
-        except ObjectNotFound:
-            msg = _("Failed to fetch Alliance data for {alliance_name}").format(
-                alliance_name=char.alliance_name,
-            )
-            messages.warning(request, msg)
-            return redirect("killstats:index")
+        ally_data = provider.get_alliance(char.alliance_id)
+        alliance, __ = EveAllianceInfo.objects.get_or_create(
+            alliance_id=ally_data.id,
+            defaults={
+                "alliance_name": ally_data.name,
+                "alliance_ticker": ally_data.ticker,
+                "executor_corp_id": ally_data.executor_corp_id,
+            },
+        )
+        audit, __ = AlliancesAudit.objects.update_or_create(
+            alliance=alliance, owner=char
+        )
+        msg = _("{alliance_name} successfully added/updated to Killstats").format(
+            alliance_name=audit.alliance.alliance_name,
+        )
+    except ObjectNotFound:
+        msg = _("Failed to fetch Alliance data for {alliance_name}").format(
+            alliance_name=char.alliance_name,
+        )
+        messages.warning(request, msg)
+        return redirect("killstats:index")
 
-    __, created = AlliancesAudit.objects.update_or_create(alliance=alliance, owner=char)
-
-    if created:
-        killmail_update_ally.apply_async(args=[char.alliance_id], priority=6)
-
-    msg = f"{char.alliance_name} successfully added/updated to Killboard"
     messages.info(request, msg)
-    return redirect("killstats:index")
+    return redirect("killstats:alliance", alliance_id=alliance.alliance_id)
 
 
 @login_required
