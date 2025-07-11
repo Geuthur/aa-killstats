0188769bad8527c187d152392a08a249f889981f
Geuthur <geuthur@users.noreply.github.com>
2025-07-11 10:48:27 +0200

    [CHANGE] use `django-esi` new User Agent Guidelines

--- killstats/__init__.py
+++ killstats/__init__.py
@@ -3,6 +3,9 @@
 __version__ = "0.5.4"
 __title__ = "Killstats"
 
-APP_NAME = "killstats"
+__package_name__ = "aa-killstats"
 
-USER_AGENT_TEXT = f"{APP_NAME} v{__version__} @ devgeuthur@gmail.com"
+__app_name_useragent__ = "AA-Killstats"
+__github_url__ = f"https://github.com/Geuthur/{__package_name__}"
+
+USER_AGENT_TEXT = f"{__app_name_useragent__} v{__version__} @ {__github_url__}"
